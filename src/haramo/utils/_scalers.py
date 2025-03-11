update dependencies and replace ScalerWrapper with TransformerWrapper and using it to correct variance filter transformer

src/haramo/utils/_scalers.py
@@ -16,31 +16,11 @@ from optuna import Trial
 import pandas as pd
 import numpy as np
 
+from ..utils import TransformerWrapper
+
 #############
 # Functions #
 #############
-
-
-class ScalerWrapper:
-    def __init__(self, scaler: callable):
-        self.scaler = scaler
-
-    def fit(self, X: Union[np.ndarray, pd.DataFrame], y=None):
-        self.scaler.fit(X, y)
-        return self
-
-    def transform(self, X: Union[np.ndarray, pd.DataFrame]):
-        transformed = self.scaler.transform(X)
-        if isinstance(X, pd.DataFrame):
-            return pd.DataFrame(transformed, columns=X.columns, index=X.index)
-        elif isinstance(X, np.ndarray):
-            return transformed
-        else:
-            raise ValueError("Input must be a DataFrame or ndarray")
-
-    def fit_transform(self, X: Union[np.ndarray, pd.DataFrame], y=None):
-        self.fit(X, y)
-        return self.transform(X)
 
 
 def instantiate_standard_scaler(trial: Trial, hyperparameters: str = "optimize"):
@@ -54,7 +34,7 @@ def instantiate_standard_scaler(trial: Trial, hyperparameters: str = "optimize")
     else:
         raise ValueError("hyperparameters must be 'optimize' or 'default'")
     scaler = StandardScaler(**params)
-    return ScalerWrapper(scaler)
+    return TransformerWrapper(scaler)
 
 
 def instantiate_minmax_scaler(trial: Trial, hyperparameters: str = "optimize"):
@@ -69,7 +49,7 @@ def instantiate_minmax_scaler(trial: Trial, hyperparameters: str = "optimize"):
     else:
         raise ValueError("hyperparameters must be 'optimize' or 'default'")
     scaler = MinMaxScaler(**params)
-    return ScalerWrapper(scaler)
+    return TransformerWrapper(scaler)
 
 
 def instantiate_robust_scaler(trial: Trial, hyperparameters: str = "optimize"):
@@ -88,7 +68,7 @@ def instantiate_robust_scaler(trial: Trial, hyperparameters: str = "optimize"):
     else:
         raise ValueError("hyperparameters must be 'optimize' or 'default'")
     scaler = RobustScaler(**params)
-    return ScalerWrapper(scaler)
+    return TransformerWrapper(scaler)
 
 
 def instantiate_identity_function(trial: Trial, hyperparameters: str = "optimize"):
@@ -99,7 +79,7 @@ def instantiate_identity_function(trial: Trial, hyperparameters: str = "optimize
     else:
         raise ValueError("hyperparameters must be 'optimize' or 'default'")
     function = FunctionTransformer()
-    return ScalerWrapper(function)
+    return TransformerWrapper(function)
 
 
 def instantiate_scaler(